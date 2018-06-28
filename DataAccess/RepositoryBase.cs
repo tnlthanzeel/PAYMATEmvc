b24ff2ff77c6f7b124d7c40c5e5312e14864a0ba
tnlthanzeel <tnlthanzeel@gmail.com>
2018-06-28 20:40:16 +0530

    repository base class added

---
+++ DataAccess/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RepositoryBase
+    {
+        private readonly PaymateDB _paymateDB;
+
+        public RepositoryBase(PaymateDB paymateDB)
+        {
+            _paymateDB = paymateDB;
+        }
+
+        public PaymateDB CreateContext()
+        {
+            return _paymateDB;
+        }
+    }
+}
